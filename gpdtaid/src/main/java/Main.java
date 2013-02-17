6fdd110d74348d8bc7371ee6c6d7b049ff99b5e0
aarribas <andres.arribas@gmail.com>
2013-02-17 19:14:08 +0100

    jmatio working first example in main.

--- gpdtaid/src/main/java/Main.java
+++ gpdtaid/src/main/java/Main.java
@@ -1,3 +1,13 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream.GetField;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import com.jmatio.common.MatDataTypes;
+import com.jmatio.io.*;
+import com.jmatio.types.*;
 
 public class Main {
 
@@ -6,7 +16,35 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		MatFileReader mfr  = null;
+		
+		try {
+			mfr = new MatFileReader("/Users/andresaan/Documents/MAI/Thesis/matlab/Exercise Final/toy_y.mat");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		MLStructure data = (MLStructure) mfr.getMLArray("data");
+		
+		Collection<String> names = data.getFieldNames();
+		
+		System.out.println(names.toString());
+		
+		MLStructure nodeinfo  = (MLStructure) data.getField("node");
+		
+		double t = ((MLDouble) nodeinfo.getField("outgoingLinks", 0)).get(0);
+		
+		System.out.println(t);
+		
+		
+		
+		
+		
 	}
 
 }
